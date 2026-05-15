50549ac0e0f9ad6de9b20b02513bee0e25689e10
Ellie Noble <the.ellie.noble@gmail.com>
2026-05-15 15:12:14 -0700

    Forgot to commit properly

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.ArrayList;
+public class Main {
+
+public static void main(String[] args){
 }
+
+public  static class Node{
+    int healthMetric;
+    Node next;
+
+    public Node(int healthMetric, Node next) {
+        this.healthMetric = healthMetric;
+        this.next = next;
+
+    }
+}
+
+public boolean isHealthRecordSymetrical(Node head) {
+    Node current = head;
+    ArrayList<Integer> healthArray = new ArrayList<>();
+    while (current != null) {
+        healthArray.add( current.healthMetric);
+        current = current.next;
+    }
+
+    for (int i = 0; i < healthArray.size()/2; i++){
+        if(healthArray.get(i).equals(healthArray.get(healthArray.size() - 1 - i))){
+            return false;
+        }
+
+    }
+    return true;
+}
+
+}
+
+
